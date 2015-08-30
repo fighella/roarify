57bd81a0541b9794d3308978a1222d56c3571e2a
Jonny Utah Dalgleish <fighella@gmail.com>
2015-08-30 20:59:17 +1000

    Connected

--- lib/roarify/connect.rb
+++ lib/roarify/connect.rb
@@ -1,5 +1,7 @@
 module Roarify
   class Connect
+    attr_accessor :auth
+
     def initialize(keys) #api_key=nil, password=nil, shop_domain=nil)
       @api_key = keys[:api_key]
       @password = keys[:password]
@@ -23,18 +25,21 @@ module Roarify
       }
     end
 
-    # def self.auth
-    #   ## Perhaps some kind of initializer?
-    #   keys = { api_key: Roarify.shop_key[:api_key], shop_domain: Roarify.shop_key[:shop_domain], password: Roarify.shop_key[:password] }
-    #   Connect.new(keys)
-    # end
+    def self.auth
+      ## Perhaps some kind of initializer?
+      Connect.new(auth_keys)
+    end
 
-    # def self.keys( opts={} )
-    #   opts[:api_key]      || 'Not set.'
-    #   opts[:password]     || 'Not set.'
-    #   opts[:shop_domain]  || 'Not set.'
-    #   opts
-    # end
+    def self.set_keys( opts={} )
+      opts[:api_key]      || 'Not set.'
+      opts[:password]     || 'Not set.'
+      opts[:shop_domain]  || 'Not set.'
+      @@auth_keys = opts
+    end
+
+    def auth_keys
+      @@auth_keys
+    end
 
   end
 end
