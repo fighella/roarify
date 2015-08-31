cde46c162ffbc764aa955c7696df52dfeef652b0
Jonny Utah Dalgleish <fighella@gmail.com>
2015-08-31 14:25:15 +1000

    Inventory Policy

--- lib/roarify/variant.rb
+++ lib/roarify/variant.rb
@@ -1,6 +1,6 @@
 module Roarify
   class Variant
-    attr_accessor :id, :sku, :option1, :option2, :price, :title, :barcode, :inventory_quantity, :old_inventory_quantity, :inventory_management, :weight, :weight_unit, :grams
+    attr_accessor :id, :sku, :option1, :option2, :price, :title, :barcode, :inventory_quantity, :old_inventory_quantity, :inventory_management, :weight, :weight_unit, :grams, :inventory_policy
 
     def self.find(id)
       Finder.new(Variant,id).find
